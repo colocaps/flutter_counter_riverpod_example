30dee61ca08da77ffce48dfb66392faf7f75d738
colocaps <colocaps@gmail.com>
2022-10-07 18:05:17 -0300

    refactor counter component

--- lib/presentation/counter_component/counter_text_component.dart
+++ lib/presentation/counter_component/counter_text_component.dart
@@ -1,7 +1,6 @@
 import 'package:contador_riverpod/bussiness_logic/counter_provider/counter_provider.dart';
 import 'package:contador_riverpod/bussiness_logic/counter_provider/counter_provider_state.dart';
-import 'package:contador_riverpod/presentation/widgets/build_counter_text.dart';
-import 'package:contador_riverpod/presentation/widgets/build_warning.dart';
+import 'package:contador_riverpod/presentation/widgets/build_counter_by_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -17,17 +16,21 @@ class CounterTextComponent extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Consumer(builder: _buildByState);
+    return Consumer(builder: (contex, ref, widget) {
+      _listenByState(ref, context);
+
+      return BuildCounterByState(
+        counterProvider: _counterProvider,
+        ref: ref,
+      );
+    });
   }
 
-  Widget _buildByState(
-    BuildContext context,
-    WidgetRef ref,
-    Widget? widget,
-  ) {
-    final state = ref.watch(_counterProvider);
-
-    ref.listen<CounterState>(_counterProvider, (previousState, newState) {
+  void _listenByState(WidgetRef ref, BuildContext context) {
+    ref.listen<CounterState>(_counterProvider, (
+      previousState,
+      newState,
+    ) {
       if (newState is Message) {
         var snackBar = const SnackBar(
           content: Text(
@@ -37,29 +40,5 @@ class CounterTextComponent extends ConsumerWidget {
         ScaffoldMessenger.of(context).showSnackBar(snackBar);
       }
     });
-
-    if (state is Initial) {
-      var value = state.value;
-      return BuildCounterText(
-        text: value.toString(),
-      );
-    } else if (state is IncrementValue) {
-      var value = state.value;
-      return BuildCounterText(
-        text: value.toString(),
-      );
-    } else if (state is DecrementValue) {
-      var value = state.value;
-      return BuildCounterText(
-        text: value.toString(),
-      );
-    } else if (state is Warning) {
-      return BuildWarning(
-        counterProvider: _counterProvider,
-        ref: ref,
-      );
-    }
-
-    return Container();
   }
 }
